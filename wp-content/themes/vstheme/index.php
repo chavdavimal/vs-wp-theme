667e33fe64efc27482c7cd74f5e158c15a070827
Swami Chirag <php11@bexcodeservices.com>
2021-10-01 13:36:47 +0530

    vimal changes in index file

--- wp-content/themes/vstheme/index.php
+++ wp-content/themes/vstheme/index.php
@@ -10,8 +10,8 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package WordPress
- * @subpackage Twenty_Twenty
- * @since Twenty Twenty 1.0
+ * @subpackage vs theme
+ * @since vs theme 1.0
  */
 
 get_header();
